77681966f51d1edf179eacad0898192a67532982
kovsiien <ketio403@gmail.com>
2025-11-12 13:07:36 +0200

    chore: initialize Angular project with Material and core components

---
+++ angul-it/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Captcha } from './components/captcha/captcha';
+import { Result } from './components/result/result';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'captcha', component: Captcha },
+  { path: 'result', component: Result },
+  { path: '**', redirectTo: '' }
+];
